041c8e5b61c5f3e75eaa7c6a83af26e63c4d0545
Udeme01 <emmanueluddy2@gmail.com>
2025-02-10 10:27:15 +0100

    learnt how to get information about the incoming request and response object to use to send back a response

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
+// const fs = require("fs");
 // const path = require("path");
 
-fs.writeFileSync("output.txt", "node file hello"); // writing output to a file - using the file system function()
+// fs.writeFileSync("output.txt", "node file hello"); // writing output to a file - using the file system function()
 
-console.log("hello world"); // instead of the console...
+// console.log("hello world"); // instead of the console...
 
 // CORE MODULES
 // http - helps in launching a server, send requests
@@ -13,4 +13,20 @@ console.log("hello world"); // instead of the console...
 // os
 
 const http = require("http");
-// require either takes a path to another file(e.g. ur own JS files - imports of these start with './' or '/') or you can use a core module from one of the "CORE MODULE"
+// require either takes a path to another file(e.g. ur own JS files - imports of these start with './' or '/') or you can use a core module from one of the "CORE MODULE".
+
+// how to create an http server...
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //   process.exit();
+
+  // how to send a response backk to client...
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first nodejs page</title></head>");
+  res.write("<body><h1>Hello to my nodejs server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(8000);
